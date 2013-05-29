044c554bafacc32729e8e06c4e46927746d49444
Darrell Hamilton <darrell.noice@gmail.com>
2013-05-29 00:30:15 -0700

    added speclj dependency

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[speclj "2.5.0"]]
+  :dev-dependencies [[com.speclj/speclj "2.5.0"]]
+  :test-path "spec/")
